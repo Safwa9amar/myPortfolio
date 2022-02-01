38cb917fb614a9d3e91c13d18cf7b65538da3dda
Safwa9amar <hassanih97@gmail.com>
2022-02-01 02:20:18 +0100

    first commit

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,6 +1,7 @@
 import { H1, Text } from "../all_in_one";
 import styled from "styled-components";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 
 const Nav = styled.div`
