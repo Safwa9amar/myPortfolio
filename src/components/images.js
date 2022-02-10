0a699660268af5b5e23b7aceb3c0bb4ac50b54a8
Safwa9amar <hassanih97@gmail.com>
2022-02-10 17:37:21 +0100

    working on services page

--- src/components/images.js
+++ src/components/images.js
@@ -1,5 +1,6 @@
-import image1 from './img/interview_re_e5jn.svg'
-import image2 from './img/design_process_re_0dhf.svg'
+import image1 from "./img/interview_re_e5jn.svg";
+import image2 from "./img/design_process_re_0dhf.svg";
 
- 
-export default [image1 , image2 ];
+let arr = [image1, image2];
+
+export default arr;
