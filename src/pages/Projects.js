e4ccf93cfca5ed442be051e706419f5f4bcf618f
safwan badboy <Safwa9amar>
2022-04-16 10:02:51 +0100

    using tailwand css to custom component

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,66 +1,43 @@
-import styled from 'styled-components'
-import { Flex, TextSmall } from '../components/all_in_one';
-const Section = styled.div`
-  /* border:1px solid; */
-  margin: 1rem;
-  padding:1rem;
- `
-
-const ProjectCard = styled.div`
-  position:relative;
-  width: 80%;
-  border-radius: 15px;
-   img{
-    filter:brightness(60%);
-    height: 50%;
-    width: 50%;
-  }
-  .detail{
-    position:absolute;
-    bottom:1rem;
-    left:1rem;
-    .tag{
-      background:whitesmoke;
-      text-align:center;
-      border-radius:5px;
-      padding-inline:0.5rem;
-      font-size: .9rem;
-      width: fit-content;
-    }
-    .some_text{
-      font-size: 2rem;
-      color:white;
-      font-weight: bold;
-    }
-  }
- 
- `
-
+const Tabs = function () {
+  return (
+    <>
+      <div className="flex justify-center gap-10 m-4 text-gray-500">
+        <button>
+          Show all <sup>12</sup>
+        </button>
+        <button>
+          Show all <sup>12</sup>
+        </button>
+        <button>
+          Show all <sup>12</sup>
+        </button>
+        <button>
+          Show all <sup>12</sup>
+        </button>
+      </div>
+    </>
+  );
+};
+const Board = function () {
+  return (
+    <>
+      <div className="flex justify-center w-full">
+        <div className="relative p-4">
+          <img src="https://via.placeholder.com/720x360" />
+          <div className="absolute bottom-4 left-10 m-4">
+            <div className="rounded-xl bg-white text-center font-semibold w-1/2 text-gray-600">Tag</div>
+            <h1 className="text-xl my-3 text-white font-bold">cardBoard</h1>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 const Projects = () => {
   return (
     <>
-      <Section>
-        <Flex center>
-          <Flex around half >
-            <TextSmall>Show All <sup>10</sup> </TextSmall>
-            <TextSmall>Design <sup>4</sup></TextSmall>
-            <TextSmall>ecomerce <sup>20</sup></TextSmall>
-            <TextSmall>code <sup>3</sup></TextSmall>
-          </Flex>
-        </Flex>
-      </Section>
-      <Section>
-        <ProjectCard>
-          <img
-            src="https://image.freepik.com/photos-gratuite/gros-plan-portrait-jeune-bel-homme-affaires-pensif-confiant-travaillant-assis-table-ecrit-dans-cahier-interieur-bureau-moderne-blanc_176420-6562.jpg"
-            alt="puc"
-          />
-          <div className='detail'>
-            <p className='tag'>Design</p>
-            <div className='some_text'>Some text</div>
-          </div>
-        </ProjectCard>
-      </Section>
+      <Tabs />
+      <Board />
     </>
   );
 };
