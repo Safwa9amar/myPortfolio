6ccb55498007aafec43d3fd89bdf80bf18c2d8b1
safwan badboy <Safwa9amar>
2022-04-16 13:07:33 +0100

    using tailwand css to custom component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,11 @@
 import styled from "styled-components";
 import { AiFillGithub, AiFillFacebook, AiFillMail } from "react-icons/ai";
-import './footer.css'
+import "./footer.css";
 import { Link } from "react-router-dom";
-
 
 const Container = styled.div`
   height: 35vh;
   background-color: #28293e;
- 
 `;
 
 const Footer = () => {
@@ -34,51 +32,6 @@ const Footer = () => {
           </li>
         </ul>
       </div>
-      <div class="footer-bottom">
-        <p>H.safwan</p>
-        <div class="footer-menu">
-          <ul class="f-menu">
-            <li>
-              <Link className={Location.pathname === "/" ? "active" : ""} to="/">
-                Home
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={Location.pathname === "/Services" ? "active" : ""}
-                to="/Services"
-              >
-                Services
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={Location.pathname === "/Projects" ? "active" : ""}
-                to="/Projects"
-              >
-                Projects
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={Location.pathname === "/Blogs" ? "active" : ""}
-                to="/Blogs"
-              >
-                Blogs
-              </Link>
-            </li>
-            <li>
-              <Link
-                className={Location.pathname === "/Contact" ? "active" : ""}
-                to="/Contact"
-              >
-                Contact
-              </Link>
-            </li>
-          </ul>
-        </div>
-      </div>
-
     </Container>
   );
 };
--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,44 +1,92 @@
+import styled from "styled-components";
+
+const data = [
+  {
+    img: "https://via.placeholder.com/360x370",
+    header: "hello world !",
+    tag: "design",
+  },
+  {
+    img: "https://via.placeholder.com/720x360",
+    header: "hello again !",
+    tag: "markating",
+  },
+  {
+    img: "https://via.placeholder.com/720x360",
+    header: "hello again !",
+    tag: "Illustration",
+  },
+  {
+    img: "https://via.placeholder.com/720x360",
+    header: "hello again !",
+    tag: "Branding",
+  },
+];
+
+const CardContainer = styled.div``;
+
 const Tabs = function () {
   return (
-    <>
-      <div className="flex justify-center gap-10 m-4 text-gray-500">
-        <button>
-          Show all <sup>12</sup>
-        </button>
-        <button>
-          Show all <sup>12</sup>
-        </button>
-        <button>
-          Show all <sup>12</sup>
-        </button>
-        <button>
-          Show all <sup>12</sup>
-        </button>
-      </div>
-    </>
+    <div className="flex flex-wrap justify-center gap-10 m-4 text-gray-500">
+      <button>
+        Show all <sup>12</sup>
+      </button>
+      <button>
+        Show all <sup>4</sup>
+      </button>
+      <button>
+        Show all <sup>6</sup>
+      </button>
+      <button>
+        Show all <sup>2</sup>
+      </button>
+    </div>
   );
 };
-const Board = function () {
+const Board = function (props) {
   return (
-    <>
-      <div className="flex justify-center w-full">
-        <div className="relative p-4">
-          <img src="https://via.placeholder.com/720x360" alt='card'/>
-          <div className="absolute bottom-4 left-10 m-4">
-            <div className="rounded-xl bg-white text-center font-semibold w-1/2 text-gray-600">Tag</div>
-            <h1 className="text-xl my-3 text-white font-bold">cardBoard</h1>
+    <div className={props.style}>
+      <div className="relative p-4 h-full w-full">
+        <img className="h-full w-full" src={props.img} alt="card" />
+        <div className="absolute  bottom-2.5 left-10 m-4 xl:text-xl 2xl:text-2xl md:text-base sm:text-xs">
+          <div className="rounded-xl bg-white text-center font-semibold  text-gray-600 ">
+            {props.tag}
           </div>
+          <h1 className="my-3 text-white font-bold">{props.header}</h1>
         </div>
       </div>
-    </>
+    </div>
+  );
+};
+const BoardContainer = function (props) {
+  return (
+    <div className="w-full flex flex-wrap items-stretch  lg:flex-row md:flex-row ">
+      {props.children}
+    </div>
   );
 };
 const Projects = () => {
   return (
-    <>
+    <div className="m-16 flex flex-col ">
       <Tabs />
-      <Board />
-    </>
+      <Board
+        tag="love"
+        img={"https://via.placeholder.com/720x360"}
+        header="recent project"
+      />
+      <BoardContainer>
+        {data.map((el) => {
+          return (
+            <Board
+              img={el.img}
+              header={el.header}
+              tag={el.tag}
+              style="md:w-1/2 sm:w-full "
+            />
+          );
+        })}
+      </BoardContainer>
+    </div>
   );
 };
 
