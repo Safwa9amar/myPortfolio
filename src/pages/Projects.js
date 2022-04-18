bda1c381f71236e2f70dede4a6dd9a782560c4a6
safwan badboy <Safwa9amar>
2022-04-18 18:36:46 +0100

    working on blogs page

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,8 +4,6 @@ import img2 from "../components/img/project/3.jpg";
 import img3 from "../components/img/project/4.jpg";
 import img4 from "../components/img/project/5.jpg";
 import img5 from "../components/img/project/6.jpg";
-import img7 from "../components/img/project/8.png";
-import styled from "styled-components";
 import { motion } from "framer-motion";
 const data = [
   {
