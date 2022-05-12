1bc9718d77cd959ee42acba943f59c4d5d027500
safwan badboy <Safwa9amar>
2022-05-12 22:08:07 +0100

    updating other stuff

--- src/pages/test.jsx
+++ src/pages/test.jsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { useState } from "react";
 const Circle = ({ width, height }) => {
-  console.log(width, height);
   return (
     <div
       className={`rounded-[50%] w-${width} h-${height} border-2  m-5 bg-transparent border-black `}
@@ -10,10 +8,8 @@ const Circle = ({ width, height }) => {
 };
 
 const Test = () => {
-  const [circleDimention, setCircleDimention] = useState({
-    width: "20",
-    height: "20",
-  });
+
+   
   return (
     <div className="flex justify-center items-center">
       <Circle width="40" height="40"/>
